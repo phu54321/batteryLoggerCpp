2d990dc88cf813cf2455a7861be0bf3ff1eb3658
Park Hyunwoo <phu54321@naver.com>
2026-05-11 16:43:44 +0900

    build: fix peerdependency problem with devtool
    our chrome has it, so we don't need another embedded one.

--- viewer/vite.config.ts
+++ viewer/vite.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import { viteSingleFile } from "vite-plugin-singlefile"
 
 
@@ -10,7 +9,6 @@ import { viteSingleFile } from "vite-plugin-singlefile"
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
     viteSingleFile()
   ],
   resolve: {
